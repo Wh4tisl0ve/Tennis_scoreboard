a7591ffeb2fdc07ce06b3c340229678a4a584684
Vladislav Luchnikov <whatislov@mail.ru>
2024-09-16 21:49:50 +0300

    refactor(app/config.py): settings include runconfig and dbconfig classes

--- src/app/config.py
+++ src/app/config.py
@@ -2,9 +2,6 @@ from dataclasses import dataclass
 from dotenv import dotenv_values
 
 
-config = dotenv_values()
-
-
 @dataclass(frozen=True)
 class RunConfig:
     host: str = 'localhost'
@@ -12,16 +9,23 @@ class RunConfig:
 
 
 @dataclass(frozen=True)
-class Settings:
-    DB_HOST: str = config['DB_HOST']
-    DB_PORT: int = config['DB_PORT']
-    DB_USER: str = config['DB_USER']
-    DB_PASS: str = config['DB_PASS']
-    DB_NAME: str = config['DB_NAME']
+class DatabaseConfig:
+    __config = dotenv_values()
+    DB_HOST: str = __config['DB_HOST']
+    DB_PORT: int = __config['DB_PORT']
+    DB_USER: str = __config['DB_USER']
+    DB_PASS: str = __config['DB_PASS']
+    DB_NAME: str = __config['DB_NAME']
 
     @property
     def DATABASE_URL_pymysql(self):
         return f'mysql+pymysql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+
+@dataclass(frozen=True)
+class Settings:
+    db: DatabaseConfig = DatabaseConfig()
+    run: RunConfig = RunConfig()
 
 
 settings = Settings()
