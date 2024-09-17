2ce07ea9a74b6a08ab6a9fe21336bb24e86d2dc0
Vladislav Luchnikov <whatislov@mail.ru>
2024-09-17 23:15:36 +0300

    refactor(app/database_engine.py): update database_engine

--- src/app/database_engine.py
+++ src/app/database_engine.py
@@ -7,16 +7,15 @@ from typing import Generator
 
 class DataBaseEngine:
     def __init__(self, url: str):
-        self.engine = create_engine(url=url)
-        self.session_factory = sessionmaker(bind=self.engine,
-                                            autocommit=False)
+        self.engine = create_engine(url=url, echo=True)
+        self.session_factory = sessionmaker(bind=self.engine, autocommit=False, autoflush=False)
 
     def dispose(self) -> None:
         self.engine.dispose()
 
-    def get_session(self) -> Generator[Session, None]:
+    def get_session(self) -> Generator[Session, None, None]:
         with self.session_factory() as session:
             yield session
 
 
-db_engine = DataBaseEngine(url=settings.db.DATABASE_URL_pymysql)
+db_engine_mysql = DataBaseEngine(url=settings.db.DATABASE_URL_pymysql)
