2df8251c5d63c7001db43369f88e78afde2c0066
Vladislav Luchnikov <whatislov@mail.ru>
2024-09-17 23:14:10 +0300

    refactor(app/main.py): add created table in db

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,12 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from app.database_engine import db_engine_mysql
+from app.models import Base
+
+
+def main():
+    with db_engine_mysql.engine.begin() as conn:
+        Base.metadata.create_all(conn)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
