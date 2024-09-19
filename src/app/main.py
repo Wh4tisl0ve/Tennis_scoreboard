4f9b7fc666eaf89029eef89713b6c5945b563892
Vladislav Luchnikov <whatislov@mail.ru>
2024-09-19 23:25:02 +0300

    feat(src/app/main): add console view tennis scoreboard

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,27 @@
-from app.database_engine import db_engine_mysql
-from app.models import Base
+from app.game_logic.tennis_game import TennisGame
 
 
 def main():
-    with db_engine_mysql.engine.begin() as conn:
-        Base.metadata.create_all(conn)
+    game = TennisGame()
+
+    for i in range(5):
+        game.player1_goals()
+        print(game.get_dict())
+
+    for i in range(3):
+        game.player2_goals()
+        print(game.get_dict())
+
+    dict_game = game.get_dict()
+    print("".ljust(10) + "| player1 | player2")
+    print("-" * 32)
+    print("Match:".ljust(9) + str(dict_game['player1_match']['value']).rjust(10) + " | " +
+          str(dict_game['player2_match']['value']).rjust(10))
+    print("Sets:".ljust(9) + str(dict_game['player1_match']['player1_set']['value']).rjust(10) + " | " +
+          str(dict_game['player2_match']['player2_set']['value']).rjust(10))
+    print("Points:".ljust(9) + str(dict_game['player1_match']['player1_set']['points']).rjust(10) + " | " +
+          str(dict_game['player2_match']['player2_set']['points']).rjust(10))
 
 
 if __name__ == '__main__':
     main()
-
