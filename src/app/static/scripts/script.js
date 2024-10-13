0db75de8e934631a93edb8131944f55a31d31d81
Vladislav Luchnikov <whatislov@mail.ru>
2024-10-14 00:34:45 +0300

    feat(src\app\static\scripts): add simple script for handle match-score

--- src/app/static/scripts/script.js
+++ src/app/static/scripts/script.js
@@ -7,14 +7,30 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
+document.addEventListener('DOMContentLoaded', () => checkWinner());
 
-function clickButtonGoalHandler(player_id_goal) {
-    let player1_name = document.getElementById('player1_name');
-    let player2_name = document.getElementById('player2_name');
+function checkWinner() {
+    let match_win_value = 2;
 
-    let player1_match = document.getElementById('match_player1');
-    let player2_match = document.getElementById('match_player2');
-    alert(player1_match.value + " " + player2_match.value)
+    if (document.getElementById('match_player1').value == match_win_value) {
+        document.getElementById('player1_name').classList.add("winner");
+        disableButtonsGoals();
+    }
+    if (document.getElementById('match_player2').value == match_win_value) {
+        document.getElementById('player2_name').classList.add("winner");
+        disableButtonsGoals();
+    }
+}
+
+function disableButtonsGoals() {
+    let button_player1 = document.getElementById('player1_goal');
+    let button_player2 = document.getElementById('player2_goal');
+
+    button_player1.disabled = true;
+    button_player1.style.cursor = 'default';
+
+    button_player2.disabled = true;
+    button_player2.style.cursor = 'default';
 }
 
 
