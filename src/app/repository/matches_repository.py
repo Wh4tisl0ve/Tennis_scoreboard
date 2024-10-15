f78877f610331e091dfbc64bacc7fc07767cf530
Vladislav Luchnikov <whatislov@mail.ru>
2024-10-16 00:19:11 +0300

    feat(src\app\repository\matches_repository.py): add limit and offset in query

--- src/app/repository/matches_repository.py
+++ src/app/repository/matches_repository.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from sqlalchemy import select, update, or_
+from sqlalchemy import select, update, or_, text
 from sqlalchemy.orm import aliased
 
 from app.repository.sqlalchemy_repository import SqlAlchemyRepository
@@ -11,23 +11,32 @@ from app.models import Matches, Players
 class MatchesRepository(SqlAlchemyRepository):
     __model_name__ = Matches
 
-    def find_finished_matches_by_player_name(self, name: str) -> list[Matches]:
+    def find_finished_matches(self, name: str, current_page: int, items_per_page: int) -> list[Matches]:
         player1 = aliased(Players)
         player2 = aliased(Players)
         winner = aliased(Players)
 
-        return self._session.query(
-            Matches.uuid,
-            player1.name.label('player1_name'),
-            player2.name.label('player2_name'),
-            winner.name.label('winner')
-        ).join(
-            player1, Matches.player1_id == player1.id
-        ).join(
-            player2, Matches.player2_id == player2.id
-        ).join(
-            winner, Matches.winner_id == winner.id
-        ).filter(or_(player1.name.contains(name), player2.name.contains(name))).all()
+        query = (
+            self._session.query(
+                Matches.uuid,
+                player1.name.label('player1_name'),
+                player2.name.label('player2_name'),
+                winner.name.label('winner')
+            ).join(
+                player1, Matches.player1_id == player1.id
+            ).join(
+                player2, Matches.player2_id == player2.id
+            ).join(
+                winner, Matches.winner_id == winner.id
+            ).filter(or_(player1.name.contains(name), player2.name.contains(name)))
+            .offset((current_page - 1) * items_per_page)
+            .limit(items_per_page).all()
+        )
+
+        return query
+
+    def find_count_finished_matches(self) -> int:
+        return self._session.execute(text("SELECT COUNT(*) FROM Matches WHERE is_end_game")).scalar()
 
     def find_by_uuid(self, uuid: UUID) -> Matches:
         return self._session.execute(select(Matches).filter(Matches.uuid == uuid)).one()[0]
